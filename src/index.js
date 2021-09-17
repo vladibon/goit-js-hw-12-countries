920cd157f043ae97439970ee1e8559466b836012
Vladimir Bondar <vladi.bondar@gmail.com>
2021-09-18 02:06:25 +0300

    add  error processing

--- src/index.js
+++ src/index.js
@@ -1,40 +1,59 @@
-const debounce = require('lodash.debounce');
+import '@pnotify/core/dist/BrightTheme.css';
+import { error } from '@pnotify/core';
+import debounce from 'lodash.debounce';
 
 import countriesTemplate from './templates/countries.hbs';
 import countryTemplate from './templates/country.hbs';
+
 import fetchCountries from './js/fetchCountries';
 import refs from './js/refs';
 
 refs.input.addEventListener('input', debounce(onSearch, 500));
 
 function onSearch(e) {
-  const searchQuery = e.target.value;
+  const searchQuery = e.target.value.trim();
 
-  searchQuery &&
-    fetchCountries(searchQuery).then(appendCountriesMarkup).catch(onFetchError);
+  if (!searchQuery) {
+    clearCountriesContainer();
+    return;
+  }
+
+  fetchCountries(searchQuery)
+    .then(countries => {
+      const countriesCount = countries.length;
+      const message = countries.message;
+
+      // console.log(countries, countriesCount, message);
+
+      if (message) onError(message);
+      if (countriesCount > 10)
+        onError(`${countriesCount} matches found. Please enter a more specific query!`);
+
+      if (countriesCount > 1 && countriesCount <= 10) appendCountriesMarkup(countries);
+      if (countriesCount === 1) appendCountryMarkup(...countries);
+    })
+    .catch(onError);
 }
 
 function appendCountriesMarkup(countries) {
-  const countriesCount = countries.length;
+  refs.container.innerHTML = countriesTemplate(countries);
+}
 
-  if (countriesCount === 1) {
-    refs.container.innerHTML = countryTemplate(countries);
-  }
-
-  if (countriesCount > 1 && countriesCount < 10) {
-    refs.container.innerHTML = countriesTemplate(countries);
-  }
-
-  if (countriesCount >= 10) {
-    clearCountriesContainer();
-    onFetchError();
-  }
+function appendCountryMarkup(country) {
+  refs.container.innerHTML = countryTemplate(country);
 }
 
 function clearCountriesContainer() {
   refs.container.innerHTML = '';
 }
 
-function onFetchError(error) {
-  alert('Упс, что-то пошло не так!');
+function onError(message) {
+  clearCountriesContainer();
+
+  error({
+    text: message,
+    delay: 1000,
+    sticker: false,
+    closer: false,
+  });
 }
